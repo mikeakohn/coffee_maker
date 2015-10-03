a2dde1e9ff7eb1fad8f2f6180df8b7affda9f055
Michael Kohn <mike@mikekohn.net>
2015-10-03 12:02:09 -0500

    Checking this in before switching it to C++.  Wish C had classes :(.

--- class_writer.c
+++ class_writer.c
@@ -3,7 +3,12 @@
 
 struct _class_writer *class_writer_alloc()
 {
-  return NULL;
+  struct _class_writer *class_writer;
+
+  class_writer = (struct _class_writer *)malloc(sizeof(struct _class_writer));
+  memset(class_writer, 0, sizeof(struct _class_writer));
+
+  return class_writer;
 }
 
 int class_writer_add_field(struct _class_writer *class_writer)
--- coffee_maker.c
+++ coffee_maker.c
@@ -7,6 +7,7 @@
 int main(int argc, char *argv[])
 {
   FILE *out;
+  struct _class_writer *class_writer;
 
   printf("\ncoffee_maker by Michael Kohn\n"
          "http://www.mikekohn.net/\n\n");
@@ -25,6 +26,7 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
+  class_writer = class_writer_alloc();
 
   fclose(out);
 
